f2db042c19b22bf399a3e10d6d67a5a582e9e9e1
OmverYo <64951757+OmverYo@users.noreply.github.com>
2023-10-12 02:40:40 +0900

    새로운 프로젝트 시작
    Create React App을 이용하여 새로운 프로젝트 시작
    
    간단한 헤더와 네비 버튼 제작
    
    CSS를 이용해 헤더 디자인

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./../style.css"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h2 className="top-header">Justin Lee's Portfolio</h2>
+            <h3>About me</h3>
+            <h3>Skills</h3>
+            <h3>GitHub</h3>
+            <h3>Projects</h3>
+            <h3>Career</h3>
+        </nav>
+    )
+}
